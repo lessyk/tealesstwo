6e2732006090950e45a00535ac177976efdaf1b2
lessyk <64253064+lessyk@users.noreply.github.com>
2024-03-17 08:46:59 +0000

    tea css2

---
+++ index.js
@@ -0,0 +1,8 @@
+// Helper function to calculate the square of a number
+function square(number) {
+    return number * number;
+}
+
+// Test the helper function
+let num = 5;
+console.log("The square of", num, "is", square(num));
